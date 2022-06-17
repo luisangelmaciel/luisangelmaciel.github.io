b2ebb36956cd54183249b8a3da3630c04b8e1c81
LuisAngelMaciel <89158021+luisangelmaciel@users.noreply.github.com>
2022-06-17 18:33:07 -0500

    lamp font para logo

---
+++ lamp/lamp_icons.dart
@@ -0,0 +1,21 @@
+// Place fonts/lamp.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: lamp
+//      fonts:
+//       - asset: fonts/lamp.ttf
+import 'package:flutter/widgets.dart';
+
+class Lamp {
+  Lamp._();
+
+  static const String _fontFamily = 'lamp';
+
+  static const IconData dragvesti = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData avatar = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData xiiber_travel_log = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData logo_dragvesti = IconData(0xf257, fontFamily: _fontFamily);
+  static const IconData logo_dragvesti_blanco_letras = IconData(0xea50, fontFamily: _fontFamily);
+  static const IconData xiiber_logo_horizontal_color1 = IconData(0xea57, fontFamily: _fontFamily);
+}
