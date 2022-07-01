8d08699fbe8ad1d4bd54032e2888abea158c93cf
LuisAngelMaciel <89158021+luisangelmaciel@users.noreply.github.com>
2022-06-30 20:59:41 -0500

    random + img

--- js/random-quotes.js
+++ js/random-quotes.js
@@ -1,111 +1,86 @@
-/*****
-
-
-Deberías estar desayunando pito y no avena remojada. 
-Vamo a jugar al goloso, tú te agachas y yo te lo roso. 
-Fingir no saber para ver hasta donde llega la hipocresia de la gente. Huason. 
-Quien te quiere en su vida, vuelve a buscarte. 
-Quiero ser mi versión más sana y bonita este año. 
-No se trata de enamorarlo, sino de mantenerlo enamorado. Conquistarlo aunque ya sea tu novio
-Tener la posibilidad de herir a quien nos hirió y no hacerlo eso es lo que nos distingue de la mala gente.
-Solo puede quedar uno…
-Bedoyecta Beryaerecta
-Según científicos esta es la canción más feliz del mundo
-
-3 cosas que me encanta tomar: Malas decisiones, Alcohol, Malas decisiones bajo la influencia del alcohol 
-El día que el amor se convierta en cerveza, me lo tomaré en serio. 
--Pero dime, ¿qué  ganas bebiendo? -Nada, yo bebo sin fines de lucro. 
-Mañana cuando despiertes te vas a dar cuenta que todo pasó muy rápido. ¡Disfruta el viaje no te lo pierdas! 
-Aprovecha el fin de semana y ve a conocer #MexicoFriendly  #MéxicoIncluyente 
-“Welcome home” Entrada con el titulo. 
-Tierra trágame y escúpeme en Acapulco 
-Dicen si me sigues en Twitter, tendrás muchos viajes este año y el que viene. 
-
-Siguenos en nuestras redes para conocer los próximos viajes en grupo, fotos, videos, memes,noticias, porno y promociones. 
-Original websites, better clicks.
-@luisangelmaciel
-
-¿Qué valoración nos darías?GenialBienRegularMalFatal
-También puedes enviarnos un comentarios, si lo deseas. 
-
-Hecho en Radolfzell (Alemania) por QaamGo Media GmbH
-
-
-
-ENVÍA TÚ FOTO (EN TRAJE DE BAÑO,  ROPA INTERIOR O DESNUDO)
-CON LA LEYENDA  CG TRAVEL, Y TE DAMOS EL 50% DE DESCUENTO EN EL TRANSPORTE 
-
-Términos y condiciones
-0. El monto total a pagar es de $427.50, que incluye acceso a 
-   Tepozspa con derecho a un locker y transporte redondo. 
-1. Seguirnos en Twitter @CGTravelMexico 
-2. Subir una fotografía en traje de baño,ropa interior o desnudo 
-    con el logo de CGT
-2. Etiquetarnos con @CGTravelMexico y #16SeptiembreEnTepoz
-4. El pago de $427.50 deberá ser enviado completo antes del viaje
-5. Valido solo para las 5 personas con mayor numero de 
-    FAVs Y RTS
-
-     Cierre de votaciones hasta el viernes 8 de septiembre a la 1pm
-
-Información adicional y reservaciones 
-Los pagos de Banamex y Bancomer, los puedes realizar de Lunes a Viernes de 8am a 8pm en: 
-
-N° de plástico a nombre de Luis Maciel5482 3412 0567 2920
-
-También te invitamos a leer nuestra nueva guía con buenas prácticas para el seguimiento, calificación y automatización del embudo de ventas.
-
-Gestión y Automatización de Leads
-Espero el material genere un impacto positivo en tu gestión.
-
-
-¿Te gusta nuestro servicio gratuito?
-Valoramos sus comentarios.
-
-
-
-
-
-lamp! te hace más feliz :)
-Si sabes vender y justificar a nivel nu0mérico cualquier idea, esa idea será rentable.
-Nos costó uno y la mitad del otro comprar este espacio. Pero valió la pena que lo vieras y de paso te rieras. 
-Desarrollado con mucha paciencia, investigación y creatividad.
-*****/
+//*****
+//
+//La resaca dura un día.Los recuerdos son para siempre.
+//Si tiene tatuajes, coge rico.
+//Exigo su _ en mi _¿ Adivinen qué bombón cumple años en febrero ?
+//Soy el perverso qyue le puedes presentar a tus papás por que tengo cara de gente decente.
+//Una parte de mi quiere ir de viaje y la otra también
+//Lo que pienses, lo serás.Lo que sientas, lo atraerás.Lo que imagines, lo crearás.Buda.
+//Un minuto de silencio por los que alguna cez fuimos delgados y ahora parecemos un rico tamalito.Amén.
+//
+//Deberías estar desayunando pito y no avena remojada.
+//Vamo a jugar al goloso, tú te agachas y yo te lo roso.
+//Fingir no saber para ver hasta donde llega la hipocresia de la gente.Huason.
+//Quien te quiere en su vida, vuelve a buscarte.
+//Quiero ser mi versión más sana y bonita este año.
+//No se trata de enamorarlo, sino de mantenerlo enamorado.Conquistarlo aunque ya sea tu novio
+//Tener la posibilidad de herir a quien nos hirió y no hacerlo eso es lo que nos distingue de la mala gente.
+//Solo puede quedar uno…
+//Bedoyecta Beryaerecta
+//Según científicos esta es la canción más feliz del mundo
+//
+//3 cosas que me encanta tomar Malas decisiones, Alcohol, Malas decisiones bajo la influencia del alcohol
+//El día que el amor se convierta en cerveza, me lo tomaré en serio. -
+//Pero dime, ¿qué ganas bebiendo ? -Nada, yo bebo sin fines de lucro.
+//Mañana cuando despiertes te vas a dar cuenta que todo pasó muy rápido.¡Disfruta el viaje no te lo pierdas!
+//Aprovecha el fin de semana y ve a conocer# MexicoFriendly# MéxicoIncluyente“ Welcome home” Entrada con el titulo.
+//Tierra trágame y escúpeme en Acapulco
+//Dicen si me sigues en Twitter, tendrás muchos viajes este año y el que viene.
+//
+//Siguenos en nuestras redes para conocer los próximos viajes en grupo, fotos, videos, memes, noticias, porno y promociones.
+//Original websites, better clicks.
+//@luisangelmaciel
+//
+//¿ Qué valoración nos darías ? GenialBienRegularMalFatal
+//También puedes enviarnos un comentarios, si lo deseas.
+//
+//Hecho en Radolfzell(Alemania) por QaamGo Media GmbH
+//
+//
+//
+//ENVÍA TÚ FOTO(EN TRAJE DE BAÑO, ROPA INTERIOR O DESNUDO)
+//CON LA LEYENDA CG TRAVEL, Y TE DAMOS EL 50 % DE DESCUENTO EN EL TRANSPORTE
+//
+//Términos y condiciones
+//0. El monto total a pagar es de $427 .50, que incluye acceso a
+//Tepozspa con derecho a un locker y transporte redondo.
+//1. Seguirnos en Twitter @CGTravelMexico
+//2. Subir una fotografía en traje de baño, ropa interior o desnudo
+//con el logo de CGT
+//2. Etiquetarnos con @CGTravelMexico y #16SeptiembreEnTepoz
+//4. El pago de $427.50 deberá ser enviado completo antes del viaje
+//5. Valido solo para las 5 personas con mayor numero de 
+//FAVs Y RTS
+//
+//Cierre de votaciones hasta el viernes 8 de septiembre a la 1pm
+//
+//Información adicional y reservaciones 
+//Los pagos de Banamex y Bancomer, los puedes realizar de Lunes a Viernes de 8am a 8pm en: 
+//
+//N° de plástico a nombre de Luis Maciel5482 3412 0567 2920
+//
+//También te invitamos a leer nuestra nueva guía con buenas prácticas para el seguimiento, calificación y automatización del embudo de ventas.
+//
+//Gestión y Automatización de Leads
+//Espero el material genere un impacto positivo en tu gestión.
+//
+//
+//¿
+//Te gusta nuestro servicio gratuito ?
+    //Valoramos sus comentarios.
+//
+//
+//
+//
+//
+//lamp!te hace más feliz
+//Si sabes vender y justificar a nivel nu0mérico cualquier idea, esa idea será rentable.
+//Nos costó uno y la mitad del otro comprar este espacio.Pero valió la pena que lo vieras y de paso te rieras.
+//Desarrollado con mucha paciencia, investigación y creatividad. **
+    //
 
 $(document).ready(function() {
-    var quoteList = [
-        { author: "", source: "", quote: "La economía colaborativa es un fenómeno que avanza a nivel mundial con paso firme. De hecho según un informe sobre América Latina, del FOMIN del BID, el IE Business School y el Fondo de Cooperación de España, un 69 por ciento de iniciativas de este tipo se concentran en Brasil, México, Argentina y Perú " },
-        { author: "Andy Worhol", source: "", quote: "La idea no es vivir para siempre. La idea es crear algo que sí lo haga" },
-        { author: "", source: "", quote: "Sí buscas resultados distintos, no hagas siempre lo mismo." },
-        { author: "", source: "", quote: "No hables, actúa. No digas, demuestra. No prometas, cumple" },
-        { author: "", source: "", quote: "Nos hemos acostumbrado a decir ´estoy bien´, para ahorrarnos preguntas que no queremos contestar " },
-        { author: "", source: "", quote: "Haces 99 cosas bien, pero si haces 1 mal, se olvidan de las 99" },
-        { author: "", source: "", quote: "El storydoing se trata de fomentar la participación del usuario en la historia, que interactúe y la viva en primera persona consiguiendo que la experiencia sea lo más inmersiva posible y el efecto, reacción, respuesta y experiencia por parte del usuario sea mucho más potente y por tanto efectiva.", link: "https://www.easypromosapp.com/blog/2022/05/ejemplo-storydoing-con-captacion-datos/?utm_source=Easypromos+new+newsletter&utm_campaign=3b01ce6c5c-easypromos_2022_06_01&utm_medium=email&utm_term=0_cc4558e8cf-3b01ce6c5c-326483114" },
-        { author: "", source: "", quote: "Dejame ser tu próximo departamento de mercadotecnia y comunicación." },
-        { author: "Javier Sosa. Director de mercadotecnia de PPG-COMEX (2016)", source: "", quote: "El verdadero elemento de entendimiento entre las agencias y las empresas es el nivel de involucramiento y la visión a largo plazo entre las marcas y las agencias. " },
-        { author: "", source: "", quote: "Toma agua a lo largo del día. Aunque no lo creas, la falta adecuada de hidratación obstruye la concentración y ni mente ni creatividad fluyen eficientemente." },
-        { author: "", source: "", quote: "Es  importante empezar el día con alimentos energéticos: nueces, semillas y un buen jugo (el verde es maravilloso). Desayunar incrementa 40% tu energía (además de que acelera el metabolismo). Todo mundo lo dice, pero uno no hace caso, caray." },
-        { author: "", source: "", quote: "Yo, como Confucio, prefiero pensar que si me dedico a algo que me apasiona no tengo que trabajar ni un día de mi vida: mi amor por lo que hago lo convierte en mi hobby. I have a job that is my hobby, hence I have a jobby!" },
-        { author: "", source: "", quote: "Si lo visualizas... lo hacemos posible. Me gusta generar ideas para construir experiencias. Gracias por la oportunidad de que trabajemos juntos. LAMP! " },
-        { author: "", source: "", quote: "Conectar con experiencias para identificar, entender, hacer crecer y mantener a tus consumidores, es mi prioridad a la hora de que trabajemos juntos. LAMP!" },
-        { author: "", source: "", quote: "Contacta conmigo y te ayudaré a impactar en tus estrategias digitales. LAMP!" },
-        { author: "", source: "", quote: "En Lamp.Martech! somos una agencia chiquita pero chingona. " },
-        { author: "Eric Descombes, presidente de FCB México.", source: "", quote: "El uso de la tecnología y datos guían las decisiones. Los líderes generan un lugar de trabajo de crecimiento y desarrollo personal.  " },
-        { author: "", source: "", quote: "El tamaño de una idea no es relevante. Lo que importa es el impacto que genera." },
-        { author: "", source: "", quote: "No se trata de enamorarlo, sino de mantenerlo enamorado. Conquistarlo aunque ya sea tu novio, pasa lo mismo con los clientes. " },
-        { author: "", source: "", quote: "Tómate un descanso, te lo mereces y haz lo que mejor sabes hacer." },
-        { author: "", source: "", quote: "Estoy en CDMX, trabajo para todo el país. lamp!" },
-        { author: "", source: "", quote: "Si me buscas, es porque quieres algo diferente. lamp!" },
-        { author: "", source: "", quote: "Es increíble  qué tanto podemos hacer cuando nos ponemos de acuerdo. " },
-        { author: "Yuri Alvarado, presidente de Alvarado Molina.", source: "", quote: "Creatividad posiblemente sea visualizar el éxito donde otras personas no ven más allá de sus narices. Un creativo imagina es como una especie de Indiana Jones, alguien que anda buscando la idea perdida.  " },
-        { author: "", source: "", quote: "Creatividad visual ir más allá de una idea. Atraer consumidores por medio de la vista se vuelve más latente al considerar la apuesta creativa con la que cuentan otras marcas, así como un entorno que es acechado por numerosos anuncios que buscan acaparar las miradas del público. " },
-        { author: "Richard Branson", source: "", quote: "No se aprende a caminar siguiendo las reglas. Se aprende caminando y cayendo" },
-        { author: "Stephen Hawking", source: "", quote: "Las personas tranquilas y silenciosas son las que tienen mentes más fuertes y ruidosas" },
-        { author: "Juan Rivera, socio y director general de Llorente y Cuenca", source: "", quote: "Las relaciones públicas adoptan un papel mucho más orientado a la consultoría y a la transformación de datos en historias que generen emociones y conexiones. Está implícito tener mucha capacidad analítica para generar alternativas de acción a los clientes. No solamente es cuestión de tener una plataforma o un software que recabe toda la información de la organización sino de incluir en esa plataforma el instinto humano que permite pulverizar los datos y traducirlos en ventajas competitivas." },
-        { author: "", source: "", quote: "" },
-        { author: "", source: "", quote: "" },
-        { author: "", source: "", quote: "Sí alguién te dice que algo es imposible, no les hagas caso, está hablando de sus limitaciones, no de las tuyas." }
-    ];
+    var quoteList = [{ author: "", source: "", quote: "La economía colaborativa es un fenómeno que avanza a nivel mundial con paso firme. De hecho según un informe sobre América Latina, del FOMIN del BID, el IE Business School y el Fondo de Cooperación de España, un 69 por ciento de iniciativas de este tipo se concentran en Brasil, México, Argentina y Perú " }, { author: "Andy Worhol", source: "", quote: "La idea no es vivir para siempre. La idea es crear algo que sí lo haga" }, { author: "", source: "", quote: "Sí buscas resultados distintos, no hagas siempre lo mismo." }, { author: "", source: "", quote: "No hables, actúa. No digas, demuestra. No prometas, cumple" }, { author: "", source: "", quote: "Nos hemos acostumbrado a decir ´estoy bien´, para ahorrarnos preguntas que no queremos contestar " }, { author: "", source: "", quote: "Haces 99 cosas bien, pero si haces 1 mal, se olvidan de las 99" }, { author: "", source: "", quote: "El storydoing se trata de fomentar la participación del usuario en la historia, que interactúe y la viva en primera persona consiguiendo que la experiencia sea lo más inmersiva posible y el efecto, reacción, respuesta y experiencia por parte del usuario sea mucho más potente y por tanto efectiva.", link: "https://www.easypromosapp.com/blog/2022/05/ejemplo-storydoing-con-captacion-datos/?utm_source=Easypromos+new+newsletter&utm_campaign=3b01ce6c5c-easypromos_2022_06_01&utm_medium=email&utm_term=0_cc4558e8cf-3b01ce6c5c-326483114" }, { author: "", source: "", quote: "Dejame ser tu próximo departamento de mercadotecnia y comunicación." }, { author: "Javier Sosa. Director de mercadotecnia de PPG-COMEX (2016)", source: "", quote: "El verdadero elemento de entendimiento entre las agencias y las empresas es el nivel de involucramiento y la visión a largo plazo entre las marcas y las agencias. " }, { author: "", source: "", quote: "Toma agua a lo largo del día. Aunque no lo creas, la falta adecuada de hidratación obstruye la concentración y ni mente ni creatividad fluyen eficientemente." }, { author: "", source: "", quote: "Es  importante empezar el día con alimentos energéticos: nueces, semillas y un buen jugo (el verde es maravilloso). Desayunar incrementa 40% tu energía (además de que acelera el metabolismo). Todo mundo lo dice, pero uno no hace caso, caray." }, { author: "", source: "", quote: "Yo, como Confucio, prefiero pensar que si me dedico a algo que me apasiona no tengo que trabajar ni un día de mi vida: mi amor por lo que hago lo convierte en mi hobby. I have a job that is my hobby, hence I have a jobby!" }, { author: "", source: "", quote: "Si lo visualizas... lo hacemos posible. Me gusta generar ideas para construir experiencias. Gracias por la oportunidad de que trabajemos juntos. LAMP! " }, { author: "", source: "", quote: "Conectar con experiencias para identificar, entender, hacer crecer y mantener a tus consumidores, es mi prioridad a la hora de que trabajemos juntos. LAMP!" }, { author: "", source: "", quote: "Contacta conmigo y te ayudaré a impactar en tus estrategias digitales. LAMP!" }, { author: "", source: "", quote: "En Lamp.Martech! somos una agencia chiquita pero chingona. " }, { author: "Eric Descombes, presidente de FCB México.", source: "", quote: "El uso de la tecnología y datos guían las decisiones. Los líderes generan un lugar de trabajo de crecimiento y desarrollo personal.  " }, { author: "", source: "", quote: "El tamaño de una idea no es relevante. Lo que importa es el impacto que genera." }, { author: "", source: "", quote: "No se trata de enamorarlo, sino de mantenerlo enamorado. Conquistarlo aunque ya sea tu novio, pasa lo mismo con los clientes. " }, { author: "", source: "", quote: "Tómate un descanso, te lo mereces y haz lo que mejor sabes hacer." }, { author: "", source: "", quote: "Estoy en CDMX, trabajo para todo el país. lamp!" }, { author: "", source: "", quote: "Si me buscas, es porque quieres algo diferente. lamp!" }, { author: "", source: "", quote: "Es increíble  qué tanto podemos hacer cuando nos ponemos de acuerdo. " }, { author: "Yuri Alvarado, presidente de Alvarado Molina.", source: "", quote: "Creatividad posiblemente sea visualizar el éxito donde otras personas no ven más allá de sus narices. Un creativo imagina es como una especie de Indiana Jones, alguien que anda buscando la idea perdida.  " }, { author: "", source: "", quote: "Creatividad visual ir más allá de una idea. Atraer consumidores por medio de la vista se vuelve más latente al considerar la apuesta creativa con la que cuentan otras marcas, así como un entorno que es acechado por numerosos anuncios que buscan acaparar las miradas del público. " }, { author: "Richard Branson", source: "", quote: "No se aprende a caminar siguiendo las reglas. Se aprende caminando y cayendo" }, { author: "Stephen Hawking", source: "", quote: "Las personas tranquilas y silenciosas son las que tienen mentes más fuertes y ruidosas" }, { author: "Juan Rivera, socio y director general de Llorente y Cuenca", source: "", quote: "Las relaciones públicas adoptan un papel mucho más orientado a la consultoría y a la transformación de datos en historias que generen emociones y conexiones. Está implícito tener mucha capacidad analítica para generar alternativas de acción a los clientes. No solamente es cuestión de tener una plataforma o un software que recabe toda la información de la organización sino de incluir en esa plataforma el instinto humano que permite pulverizar los datos y traducirlos en ventajas competitivas." }, { author: "", source: "", quote: "" }, { author: "", source: "", quote: "" }, { author: "", source: "", quote: "Sí alguién te dice que algo es imposible, no les hagas caso, está hablando de sus limitaciones, no de las tuyas." }];
 
     // var quoteRandomize = '';
     // var quoteMain = '';
