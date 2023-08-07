63b2fe778a1af5ceda10fa64f035800d39677329
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2023-08-07 13:07:04 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-var cacheName = 'lamp!';
-var filesToCache = [
-    '/',
-    '/manifest.json',
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('pwa-assets').then(cache => 
+      {
+        return cache.addAll([
+   '/manifest.json',
     '/index.html',
     '/404.html',
     '/css/styles.css',
@@ -9,22 +12,19 @@ var filesToCache = [
     '/js/main.js',
     '/js/entradas.js',
     '/js/lazysizes.min.js'
-];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+        ])
+      })
+  );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
+
 });
