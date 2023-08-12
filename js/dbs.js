b9f9f3af717785cd2ecc469528a0fe48ede052d3
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2023-08-11 23:07:59 -0600

    Update dbs.js

--- js/dbs.js
+++ js/dbs.js
@@ -1,4 +1,17 @@
-var myObject= {"codepen2023":[
+{
+  "cd" : [
+    { "title" : "Empire Burlesque", "artist" : "Bob Dylan", "price" : "10.90" },
+    { "title" : "Hide your heart", "artist" : "Bonnie Tyler", "price" : "9.90" },
+    { "title" : "Greatest Hits", "artist" : "Dolly Parton", "price" : "9.90" },
+    { "title" : "Still got the blues", "artist" : "Gary Moore", "price" : "10.20" },
+    { "title" : "Eros", "artist" : "Eros Ramazzotti", "price" : "9.90" },
+    { "title" : "One night only", "artist" : "Bee Gees", "price" : "10.90" },
+    { "title" : "Sylvias Mother", "artist" : "Dr.Hook", "price" : "8.10" }
+   ]
+}
+
+{
+  "codepen2023":[
   {"Pen":"Status indicators in CSS",
   "URL":"https://codepen.io/luisangelmaciel/pen/NWERjjv",
   "Creado":"21 Junio 2023"},
