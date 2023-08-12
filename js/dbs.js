de4c70370ac7b8ad1fd3014eb7cd1c384d0e37d8
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2023-08-11 23:14:13 -0600

    Update dbs.js

--- js/dbs.js
+++ js/dbs.js
@@ -1,37 +1,4 @@
 {
-"customers":[
-{"CustomerName" : "Alfreds Futterkiste","City" : "Berlin","Country" : "Germany"},
-{"CustomerName" : "Berglunds snabbköp","City" : "Luleå","Country" : "Sweden"},
-{"CustomerName" : "Centro comercial Moctezuma","City" : "México D.F.","Country" : "Mexico"},
-{"CustomerName" : "Ernst Handel","City" : "Graz","Country" : "Austria"},
-{"CustomerName" : "FISSA Fabrica Inter. Salchichas S.A.","City" : "Madrid","Country" : "Spain"},
-{"CustomerName" : "Galería del gastrónomo","City" : "Barcelona","Country" : "Spain"},
-{"CustomerName" : "Island Trading","City" : "Cowes","Country" : "UK"},
-{"CustomerName" : "Königlich Essen","City" : "Brandenburg","Country" : "Germany"},
-{"CustomerName" : "Laughing Bacchus Wine Cellars","City" : "Vancouver","Country" : "Canada"},
-{"CustomerName" : "Magazzini Alimentari Riuniti","City" : "Bergamo","Country" : "Italy"},
-{"CustomerName" : "North/South","City" : "London","Country" : "UK"},
-{"CustomerName" : "Paris spécialités","City" : "Paris","Country" : "France"},
-{"CustomerName" : "Rattlesnake Canyon Grocery","City" : "Albuquerque","Country" : "USA"},
-{"CustomerName" : "Simons bistro","City" : "København","Country" : "Denmark"},
-{"CustomerName" : "The Big Cheese","City" : "Portland","Country" : "USA"},
-{"CustomerName" : "Vaffeljernet","City" : "Århus","Country" : "Denmark"},
-{"CustomerName" : "Wolski Zajazd","City" : "Warszawa","Country" : "Poland"}
-]}
-
-{
-  "cd" : [
-    { "title" : "Empire Burlesque", "artist" : "Bob Dylan", "price" : "10.90" },
-    { "title" : "Hide your heart", "artist" : "Bonnie Tyler", "price" : "9.90" },
-    { "title" : "Greatest Hits", "artist" : "Dolly Parton", "price" : "9.90" },
-    { "title" : "Still got the blues", "artist" : "Gary Moore", "price" : "10.20" },
-    { "title" : "Eros", "artist" : "Eros Ramazzotti", "price" : "9.90" },
-    { "title" : "One night only", "artist" : "Bee Gees", "price" : "10.90" },
-    { "title" : "Sylvias Mother", "artist" : "Dr.Hook", "price" : "8.10" }
-   ]
-}
-
-{
   "codepen2023":[
   {"Pen":"Status indicators in CSS",
   "URL":"https://codepen.io/luisangelmaciel/pen/NWERjjv",
