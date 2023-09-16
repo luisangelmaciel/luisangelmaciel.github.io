134029e61a6d773ffeede790f90fc9a279ed53f7
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2023-09-16 17:57:41 -0600

    Add files via upload

---
+++ js/examples.js
@@ -0,0 +1,83 @@
+	var myObject = {
+			"myproyectos": [{
+					"img": "https://ucarecdn.com/6f0c8175-82e0-40ce-8705-5c6a0120fe79/misheinplayerav4.svg",
+					"URL": "https://ucarecdn.com/6f0c8175-82e0-40ce-8705-5c6a0120fe79/misheinplayerav4.svg",
+					"alt": "misheinplayerav4"
+				},
+				{
+					"img": "https://ucarecdn.com/2e4a5818-7b88-41e3-8d6f-74f8e7b0e386/misheinplayerav2.svg",
+					"URL": "https://ucarecdn.com/2e4a5818-7b88-41e3-8d6f-74f8e7b0e386/misheinplayerav2.svg",
+					"alt": "misheinplayerav4"
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "",
+					"URL": "",
+					"alt": ""
+				},
+				{
+					"img": "https://ucarecdn.com/0d1b3a90-b655-4785-bdaa-12f13ef1f75d/misheinplayerav3.svg",
+					"URL": "https://ucarecdn.com/0d1b3a90-b655-4785-bdaa-12f13ef1f75d/misheinplayerav3.svg",
+					"alt": "misheinplayerav4"
+				}
+			],
+			"myApps": [{
+				"img": "https://ucarecdn.com/6f0c8175-82e0-40ce-8705-5c6a0120fe79/misheinplayerav4.svg",
+				"URL": "https://ucarecdn.com/6f0c8175-82e0-40ce-8705-5c6a0120fe79/misheinplayerav4.svg",
+				"alt": "misheinplayerav4"
+			}]
+		};
